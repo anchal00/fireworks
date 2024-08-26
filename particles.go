9bb26c35005e0a09725dc353b563b2c05ab588ab
anchal00 <anchal82199@gmail.com>
2024-08-27 00:41:43 +0530

    Replace with Circles(kind-of)

--- particles.go
+++ particles.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"math/rand"
 	"os"
 	"time"
@@ -15,77 +16,52 @@ const (
 
 func getRandomColour() string {
 	colours := []string{
-		"\033[0m",
-		"\033[30m",
-		"\033[31m",
-		"\033[32m",
-		"\033[33m",
-		"\033[34m",
-		"\033[35m",
-		"\033[36m",
-		"\033[37m",
+		"\033[30m", // Black text
+		"\033[31m", // Red text
+		"\033[32m", // Green text
+		"\033[33m", // Yellow text
+		"\033[34m", // Blue text
+		"\033[35m", // Magenta text
+		"\033[36m", // Cyan text
+		"\033[37m", // White text
 	}
 	return colours[rand.Intn(len(colours))]
 }
 
-func renderFirework(xCoord, yCoord int, window *[ROWS][COLS]string, depth int, writer *bufio.Writer) {
-	offsets := [8][2]int{
-		{0, 1},
-		{0, -1},
-		{-1, 0},
-		{1, 0},
-		{-1, -1},
-		{-1, 1},
-		{1, -1},
-		{1, 1},
-	}
-	queue := [][]int{{xCoord, yCoord}}
-
-	for depth >= 0 {
-		size := len(queue)
-		if size == 0 {
-			return
-		}
-		for i := 0; i < size; i++ {
-			cur_cell := queue[0]
-			cur_cell_x, cur_cell_y := cur_cell[0], cur_cell[1]
-			queue = queue[1:]
-      window[cur_cell_x][cur_cell_y] = fmt.Sprintf("%v%v", getRandomColour() ,"x")
-
-			for _, offset := range offsets {
-				rowOffset, colOffset := offset[0], offset[1]
-				newX, newY := rowOffset+cur_cell_x, colOffset+cur_cell_y
-				if newX < 0 || newX >= len(window) || newY < 0 || newY >= len(window[0]) {
-					continue
+func renderFirework(cx, cy int, window *[ROWS][COLS]string, radius int, writer *bufio.Writer) {
+	for i := 1; i <= radius; i++ {
+		colour := getRandomColour()
+		for y := 0; y < COLS; y++ {
+			for x := 0; x < ROWS; x++ {
+				dx := x - cx
+				dy := y - cy
+				distance := math.Sqrt(float64(dx*dx + dy*dy))
+				if distance <= float64(i) {
+					window[x][y] = fmt.Sprintf("%v\033[1m%v", colour, "+") // \033[1m makes text bold
 				}
-				if window[newX][newY] == "x" {
-					continue
-				}
-				queue = append(queue, []int{newX, newY})
 			}
 		}
-		// render here
 		write(writer, window)
-		depth--
 		time.Sleep(200 * time.Millisecond)
 	}
 }
 
 func write(writer *bufio.Writer, cells *[ROWS][COLS]string) {
-	fmt.Fprint(writer, "\033[H")
 	for i := 0; i < ROWS; i++ {
 		for j := 0; j < COLS; j++ {
 			fmt.Fprintf(writer, "%v ", cells[i][j])
 		}
 		fmt.Fprintln(writer)
 	}
-	writer.Flush()
+	fmt.Fprint(writer, "\033[H\033[0m") // "\033[0m" -> Reset text formatting
+	writer.Flush()                      // Flush the clear screen command immediately
 }
 
 func render() {
 	writer := bufio.NewWriter(os.Stdout)
+
 	for {
-		fmt.Fprint(writer, "\033[H") /* Write ANSI escape sequence to move the cursor to top left corner. Although, this
+		fmt.Fprint(writer, "\033[H\033[0m") /* Write ANSI escape sequence to move the cursor to top left corner. Although, this
 		   sequence is not meant for clearing the screen but it works for me ¯\_(ツ)_/¯ i.e
 		   clears the screen. And the actual sequence \033[2J that should clear the screen
 		   doesn"t work. */
